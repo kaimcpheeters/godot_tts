b625a79f163b0d5151fe9a0e5fac211a59a70e64
Kai McPheeters <kaimcpheeters@gmail.com>
2024-01-12 20:53:51 -0500

    Style guide changes

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,11 @@
 extends Node
 signal tts_generated_speech
 
-const ELEVENLABS_API_KEY : String = ""
+const ELEVENLABS_API_KEY : String = "907bc085fbd00d0576f7fea0fa8748fc"
 const MALE_VOICE_CODE : String = "N2lVS1w4EtoT3dr4eOWO"
 const FEMALE_VOICE_CODE : String = "XB0fDUnXU5powFXDhCwa"
 const ENDPOINT : String = "https://api.elevenlabs.io/v1/text-to-speech/"
+const TTS_AUDIO_PATH = "user://tts_audio.mp3"
 
 var character_code : String = MALE_VOICE_CODE
 var use_stream_mode : bool = false
@@ -20,7 +21,7 @@ func _initialize():
 	# Create httprequest node
 	http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.set_download_file("user://elevenlabs.mp3")
+	http_request.set_download_file(TTS_AUDIO_PATH)
 	http_request.connect("request_completed", self, "_on_request_completed")
 	
 	# Create audio player node for speech playback
@@ -55,10 +56,10 @@ func _call_elevenlabs(text):
 		push_error("Something Went Wrong!")
 		print(error)
 		
-func _on_request_completed(result, responseCode, headers, body):
+func _on_request_completed(result, response_code, headers, body):
 	# Should recieve 200 if all is fine; if not print code
-	if responseCode != 200:
-		print("There was an error, response code:" + str(responseCode))
+	if response_code != 200:
+		print("There was an error, response code:" + str(response_code))
 		print(result)
 		print(headers)
 		print(body)
@@ -67,7 +68,7 @@ func _on_request_completed(result, responseCode, headers, body):
 	var audio_file_from_eleven = body
 	
 	var file = File.new()
-	var err = file.open("user://elevenlabs.mp3", File.READ)
+	var err = file.open(TTS_AUDIO_PATH, File.READ)
 	var bytes = file.get_buffer(file.get_len())
 	audio_stream.data = bytes 
 	audio_stream_player.set_stream(audio_stream)
